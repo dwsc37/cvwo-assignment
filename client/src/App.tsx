ed7add319721cc445e929d49369f21193a4b2c1e
dwsc37 <davewongsc@gmail.com>
2024-01-08 03:02:41 +0800

    imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,19 @@
 import { ThemeProvider } from '@mui/material';
-import { AnimatePresence } from 'framer-motion';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import WrappedRoutes from './components/Routes';
 import store from './redux/store';
 import AppTheme from './styles/AppTheme';
-import WrappedRoutes from './components/Routes';
 
 function App() {
   return (
     <ThemeProvider theme={AppTheme}>
       <Provider store={store}>
         <Toaster toastOptions={{duration: 1500}}/>
-            <AnimatePresence mode="wait">
               <BrowserRouter>
                 <WrappedRoutes />
               </BrowserRouter>
-            </AnimatePresence>
       </Provider>
     </ThemeProvider>
   );
